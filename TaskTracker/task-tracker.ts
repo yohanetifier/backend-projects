c32b02e1fe5c2b54a82659ec7b0c03eaca905777
yohan <yohan@adelios.fr>
2024-10-25 12:14:01 +0200

    Project Task Tracker: add typescript

---
+++ TaskTracker/task-tracker.ts
@@ -0,0 +1,163 @@
+import readline from 'node:readline';
+import fs from 'node:fs';
+import allTasks from './tasks.json' assert { type: 'json' };
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+type Task = {
+	id: number;
+	description: string;
+	status: string;
+	createdAt?: Date;
+	updatedAt?: Date | null;
+};
+
+type Action = 'add' | 'update' | 'delete';
+
+const STATUS = ['todo', 'in-progress', 'done'];
+
+const writeInfiles = (withAllTasks: boolean, content: Task | Task[]) => {
+	if (withAllTasks === true) {
+		fs.writeFile(
+			'./tasks.json',
+			JSON.stringify([...allTasks, content]),
+			(err: any) => {
+				if (err) {
+					console.error(err);
+				} else {
+					console.log('Task added successfully ');
+				}
+			}
+		);
+	} else {
+		fs.writeFile('./tasks.json', JSON.stringify(content), (err: any) => {
+			if (err) {
+				console.error(err);
+			} else {
+				console.log('Task added successfully ');
+			}
+		});
+	}
+
+	rl.close();
+};
+
+const addTask = (task: string) => {
+	let lastTaskId: any;
+	let taskToAdd: Task;
+
+	if (!task) {
+		console.log('You have to give a task');
+		rl.close();
+	} else {
+		if (allTasks) {
+			lastTaskId = allTasks.slice(-1).map((e: any) => e.id);
+		}
+		if (lastTaskId) {
+			let lastTaskParse = JSON.parse(lastTaskId);
+			console.log('lastTaskParse', lastTaskParse);
+			lastTaskParse++;
+			taskToAdd = {
+				id: lastTaskParse,
+				description: task,
+				status: STATUS[0],
+				createdAt: new Date(),
+				updatedAt: null
+			};
+		} else {
+			taskToAdd = {
+				id: 1,
+				description: task,
+				status: STATUS[0],
+				createdAt: new Date(),
+				updatedAt: null
+			};
+		}
+		writeInfiles(true, taskToAdd);
+	}
+};
+
+const updateTasksById = (newDescription: string, id: string) => {
+	if (!newDescription) {
+		console.log('NewDescription needed !');
+	} else {
+		const shallowCopy = allTasks.slice();
+		let filterTaskArray = shallowCopy.filter(
+			(t: any) => t.id === parseInt(id)
+		);
+		const taskToUpdate = filterTaskArray.map((t: any) => ({
+			...t,
+			description: newDescription
+		}));
+		const indexOfTheTaskToUpdate = allTasks.findIndex(
+			(task: any) => task.id === parseInt(id)
+		);
+		if (indexOfTheTaskToUpdate !== -1) {
+			shallowCopy[indexOfTheTaskToUpdate] = taskToUpdate[0];
+		}
+		writeInfiles(false, shallowCopy as unknown as Task[]);
+		fs.writeFile('./tasks.json', JSON.stringify(shallowCopy), (err) => {
+			if (err) {
+				console.log(err);
+			} else {
+				console.log(`Tasks ${id} updated`);
+			}
+		});
+		rl.close();
+	}
+};
+
+rl.question('What do you want to do ? ', (action: string) => {
+	if (!action) {
+		console.log('the action can be add | update | delete');
+		rl.close();
+	}
+	if (action === 'add') {
+		rl.question('Task to add : ', (task: string) => {
+			addTask(task);
+		});
+	} else if (action === 'update') {
+		rl.question('Which task you want to update : ', (id: string) => {
+			if (!id) {
+				console.log('Id needed !');
+				rl.close();
+			} else {
+				rl.question(
+					'What is the new description : ',
+					(newDescription: string) => {
+						updateTasksById(newDescription, id);
+					}
+				);
+			}
+		});
+	} else if (action === 'delete') {
+		rl.question(
+			'Which tasks do you want to delete: ',
+			(deletedTasksId: string) => {
+				const shallowCopy = allTasks.slice();
+				const othersTasks = shallowCopy.filter(
+					(task: any) => task.id !== parseInt(deletedTasksId)
+				);
+				fs.writeFile(
+					'./tasks.json',
+					JSON.stringify([...othersTasks]),
+					(err: any) => {
+						if (err) {
+							console.log(err);
+						} else {
+							console.log(`Tasks ${deletedTasksId} deleted !`);
+							rl.close();
+						}
+					}
+				);
+			}
+		);
+	} else {
+		console.log('allow action add | update | delete');
+		rl.close();
+	}
+	// rl.close()
+});
