57740bfb37e47f731171b4cafcca23151063ffb9
yohan <yohan@adelios.fr>
2024-11-12 20:19:54 +0100

    Weather APi: Setup server

--- WeatherApi/app.ts
+++ WeatherApi/app.ts
@@ -1,9 +1,15 @@
-import express, { Response, Request, Express } from 'express';
+import express, { Response, Request, Express, NextFunction } from 'express';
 
 const app: Express = express();
 
-app.use((req: Request, res: Response) => {
-	res.json({ message: 'Request sent' });
-});
+app.use(express.json());
+
+app.post(
+	'http://locahost:3000/:postCode',
+	(req: Request, res: Response, next: NextFunction) => {
+		console.log('req.body', req.body);
+		next();
+	}
+);
 
 export default app;
--- WeatherApi/index.ts
+++ WeatherApi/index.ts
@@ -1,13 +1,49 @@
-import dotenv from 'dotenv';
 import http from 'http';
 import app from './app';
 
-dotenv.config();
+const normalizePort = (val: any) => {
+	const port = parseInt(val, 10);
 
-const PORT = process.env.PORT || 3000;
-app.set('port', PORT);
+	if (isNaN(port)) {
+		return val;
+	}
+	if (port >= 0) {
+		return port;
+	}
+	return false;
+};
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+const errorHandler = (error: any) => {
+	if (error.syscall !== 'listen') {
+		throw error;
+	}
+	const address = server.address();
+	const bind =
+		typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+	switch (error.code) {
+		case 'EACCES':
+			console.error(bind + ' requires elevated privileges.');
+			process.exit(1);
+			break;
+		case 'EADDRINUSE':
+			console.error(bind + ' is already in use.');
+			process.exit(1);
+			break;
+		default:
+			throw error;
+	}
+};
+
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-	console.log(`Server listening on port ${PORT}`);
+server.on('error', errorHandler);
+server.on('listening', () => {
+	const address = server.address();
+	const bind =
+		typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+	console.log('Listening on ' + bind);
 });
+
+server.listen(port);
