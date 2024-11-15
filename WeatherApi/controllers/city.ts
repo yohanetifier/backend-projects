026e956d25a5bfa15317a31363349bdd0682dc8a
yohan <yohan@adelios.fr>
2024-11-15 09:59:00 +0100

    Weather Api: handle the cache With Redis and add Docker compose to launch Redis

--- WeatherApi/controllers/city.ts
+++ WeatherApi/controllers/city.ts
@@ -2,13 +2,14 @@ import { NextFunction, Request, Response } from 'express';
 import { client } from '../app';
 
 type Days = {
-	adress: string;
-	tempmax: number;
-	tempmin: number;
+	address: string;
+	tempMax: number;
+	tempMin: number;
 	temp: number;
 	sunrise: string;
 	sunset: string;
 	description: string;
+	date: string;
 };
 
 export const detailsCityCtrl = async (
@@ -17,35 +18,48 @@ export const detailsCityCtrl = async (
 	next: NextFunction
 ) => {
 	const { city } = req.body;
-	const endpoints = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city},UK?key=UBH5HDB3WDYUKSLEFZRA6PBEV`;
-	const checkIfCityIsInTheCache = await client.hGetAll(`${city}`);
-
-	if (!!checkIfCityIsInTheCache) {
-		console.log('city exist');
-		const data = JSON.stringify(checkIfCityIsInTheCache);
-		res.send(data);
+	const normalizeCity = city.toLowerCase();
+	if (!normalizeCity) {
+		res.status(500).json({ message: 'No city provided' });
 	} else {
-		console.log('city not exist');
-		const response = await fetch(endpoints);
-		const data = await response.json();
-		const days: Days = data.days[0];
-		const daysDetails: { [x: string]: string } = {
-			address: `${days.adress}`,
-			tempMax: `${days.tempmax}`,
-			tempMin: `${days.tempmin}`,
-			temp: `${days.temp}`,
-			description: `${days.description}`,
-			sunrise: `${days.sunrise}`,
-			sunset: `${days.sunset}`
-		};
-		console.log('data.adress', data.resolvedAddress);
-		console.log('data.tempMax', days.tempmax);
-		console.log('data.tempMin', days.tempmin);
-		console.log('data.temp', days.temp);
-		console.log('data.description', days.description);
-		console.log('data.sunrise', days.sunrise);
-		console.log('data.sunset', days.sunset);
-		await client.hSet(`${city}`, daysDetails);
-		res.status(201).json({ data });
+		const checkIfCityIsInTheCache = await client.hGetAll(
+			`${normalizeCity}`
+		);
+		const isPresentInCache = Object.hasOwn(
+			checkIfCityIsInTheCache,
+			'address'
+		);
+
+		if (isPresentInCache) {
+			const data = JSON.stringify(checkIfCityIsInTheCache);
+			res.send(data);
+		} else {
+			try {
+				const endpoints = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=UBH5HDB3WDYUKSLEFZRA6PBEV`;
+				const response = await fetch(endpoints);
+				const data = await response.json();
+				if (data.days[0]) {
+					const days = data.days[0];
+					const daysDetails: Days = {
+						address: `${data.resolvedAddress}`,
+						tempMax: Number(`${days.tempmax}`),
+						tempMin: Number(`${days.tempmin}`),
+						temp: Number(`${days.temp}`),
+						description: `${days.description}`,
+						sunrise: `${days.sunrise}`,
+						sunset: `${days.sunset}`,
+						date: `${days.datetime}`
+					};
+					await client.hSet(`${normalizeCity}`, daysDetails);
+					res.status(201).json({ data });
+				} else {
+					res.status(500).json({
+						message: 'No data supplied for this location'
+					});
+				}
+			} catch (error) {
+				res.status(500).json({ error: error });
+			}
+		}
 	}
 };
