f3bdbf2efa5018346c8e5e53c82529f11b93ecf1
yohan <yohan@adelios.fr>
2024-11-21 22:45:13 +0100

    TodoList API: start authguard

---
+++ Todo List API/src/modules/auth/auth.guard.ts
@@ -0,0 +1,10 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+  canActivate(context: ExecutionContext): any {
+    console.log('context', context);
+  }
+}
