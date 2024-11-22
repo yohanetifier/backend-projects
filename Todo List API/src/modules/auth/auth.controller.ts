959ea43dd82c8b7d3dad42d0142761bff4070d10
yohan <yohan@adelios.fr>
2024-11-22 07:10:12 +0100

    TodoList API: implement authguard

--- Todo List API/src/modules/auth/auth.controller.ts
+++ Todo List API/src/modules/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { GetUserDTO } from '../user/dto/get-user.dto';
 import { CreateUserDTO } from '../user/dto/create-user.dto';
+import { AuthGuard } from './auth.guard';
 
 @Controller()
 export class AuthController {
@@ -13,5 +14,10 @@ export class AuthController {
   @Post('register')
   signUp(@Body() user: CreateUserDTO) {
     return this.authService.signUp(user);
+  }
+  @Post('todos')
+  @UseGuards(AuthGuard)
+  createTodo() {
+    console.log('in create todo');
   }
 }
--- Todo List API/src/modules/auth/auth.guard.ts
+++ Todo List API/src/modules/auth/auth.guard.ts
@@ -1,10 +1,31 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private jwtService: JwtService) {}
-  canActivate(context: ExecutionContext): any {
-    console.log('context', context);
+  async canActivate(context: ExecutionContext): Promise<any> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: process.env.JWT_SECRET_KEY,
+      });
+      request['user'] = payload;
+      console.log('request.user', request.user);
+    } catch {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+  private extractTokenFromHeader(request: Request) {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
   }
 }
