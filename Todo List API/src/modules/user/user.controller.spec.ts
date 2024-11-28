d7c4427f98895dc1f1a9d6c254f1868deba1344b
yohan <yohan@adelios.fr>
2024-11-28 21:47:56 +0100

    TodoList API: setup test

--- Todo List API/src/modules/user/user.controller.spec.ts
+++ Todo List API/src/modules/user/user.controller.spec.ts
@@ -1,23 +1,48 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './user.controller';
-// import { AppService } from '../../app.service';
+import { Test } from '@nestjs/testing';
 import { UserService } from './application/user.service';
 import { AuthController } from '../auth/auth.controller';
+import { GetUserDTO } from './dto/get-user.dto';
+import { User } from './domain/user.entity';
+import { PrismaUserRepository } from './infrastructure/prisma.user.repository';
+import { PrismaService } from 'src/prisma.service';
+
+jest.mock('../../../src/prisma.service.ts');
 
 describe('AppController', () => {
   let userService: UserService;
+  let authController: AuthController;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
-    let moduleRef = await Test.createTestingModule({
-      providers: [UserService],
+    const moduleRef = await Test.createTestingModule({
+      providers: [
+        UserService,
+        PrismaService,
+        // {
+        //   provide: 'PrismaUserRepository',
+        //   useClass: PrismaUserRepository,
+        // },
+      ],
       controllers: [AuthController],
     }).compile();
     userService = moduleRef.get<UserService>(UserService);
+    authController = moduleRef.get<AuthController>(AuthController);
+    authController = moduleRef.get<AuthController>(AuthController);
+    prisma = moduleRef.get<PrismaService>(PrismaService);
   });
 
   describe('getUsers', () => {
-    it('should return a user', () => {
-      const user = {};
+    it('should return an array of  user', async () => {
+      const userData: GetUserDTO = { email: 'test@test.fr', password: 'test' };
+      const result: User = {
+        id: 63636,
+        email: userData.email,
+        password: userData.password,
+        name: 'test',
+      };
+      jest.spyOn(userService, 'getUser').mockImplementation(async () => result);
+
+      expect(await authController.signIn(result)).toBe(result);
     });
   });
 });
