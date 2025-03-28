7a3af05fbaf28b78101f86d713a5c1162418a0db
yohan <yohan@adelios.fr>
2025-03-28 06:55:45 +0100

    feat: setting validator to check if email already exist

--- TodoListAPI/src/common/validators/is-email-unique.validator.ts
+++ TodoListAPI/src/common/validators/is-email-unique.validator.ts
@@ -1,5 +1,18 @@
 import { Injectable } from '@nestjs/common';
-import { ValidatorConstraintInterface } from 'class-validator';
+import { User } from '@prisma/client';
+import {
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
+import { UserService } from 'src/modules/user/application/user.service';
 
+@ValidatorConstraint({ async: true })
 @Injectable()
-export class IsEmailUnique implements ValidatorConstraintInterface {}
+export class IsEmailUnique implements ValidatorConstraintInterface {
+  constructor(private user: UserService) {}
+
+  //   async validate(user: User) {
+  //     const isExistingUser = await this.user.getUser(user.email);
+  //     return !!isExistingUser;
+  //   }
+}
