29f59861b48e0bf36801374e3abea5b682cd1fb9
yohan <yohan@adelios.fr>
2025-04-03 06:49:13 +0200

    feat: add authentication guard and JWT module to TodoController

--- TodoListAPI/src/modules/todo/todo.controller.ts
+++ TodoListAPI/src/modules/todo/todo.controller.ts
@@ -1,13 +1,29 @@
-import { Body, Controller, Headers, Post, Req } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Headers,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { TodoService } from './application/todo.service';
 import { CreateTodoDTO } from './dto/create-todo.dto';
+import { AuthGuard } from '../auth/auth.guard';
 
 @Controller()
 export class TodoController {
   constructor(private readonly postService: TodoService) {}
-  @Post()
-  createTodo(@Req() req, @Body() todo: CreateTodoDTO, @Headers() header: any) {
+  // @Post()
+  // createTodo(@Req() req, @Body() todo: CreateTodoDTO, @Headers() header: any) {
+  //   const { sub } = req.user;
+  //   return this.postService.createTodo(sub, todo);
+  // }
+  @Post('todos')
+  @UseGuards(AuthGuard)
+  createTodo(@Req() req, @Body() todo: CreateTodoDTO) {
     const { sub } = req.user;
-    return this.postService.createTodo(sub, todo);
+    console.log('todo', todo);
+    console.log('sub', sub);
+    // return this.authService.createTodo(sub, todo);
   }
 }
--- TodoListAPI/src/modules/todo/todo.module.ts
+++ TodoListAPI/src/modules/todo/todo.module.ts
@@ -3,8 +3,13 @@ import { TodoService } from './application/todo.service';
 import { TodoController } from './todo.controller';
 import { PrismaTodoRepository } from './infrastructure/prisma-todo.repository';
 import { PrismaService } from '../../prisma.service';
-
+import { JwtModule } from '@nestjs/jwt';
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET_KEY,
+    }),
+  ],
   providers: [
     TodoService,
     PrismaService,
